e45a575e98286b115abe947c60b5b69847961336
REUsha007 <134149384+REUsha007@users.noreply.github.com>
2023-06-08 18:18:07 +0530

    Updated amsd.sh

--- amsd.sh
+++ amsd.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-echo "Enter the 1st number:"
-read no1
-echo "Enter the 2nd number:"
-read no2
-sum=`expr $no1 + $no2`
-sub=`expr $no1 - $no2`
-mul=`expr $no1 \* $no2`
-div=`expr $no1 / $no2`
+sum=`expr $1 + $2`
+sub=`expr $1 - $2`
+mul=`expr $1 \* $2`
+div=`expr $1 / $2`
 echo "The sum of $no1 and $no2 is $sum"
 echo "The sub btw $no1 and $no2 is $sub"
 echo "The mul of $no1 and $no2 is $mul"
