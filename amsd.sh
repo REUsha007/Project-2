d806ee81b8e3e4e1facc815f42dc7519138e40f2
REUsha007 <134149384+REUsha007@users.noreply.github.com>
2023-06-08 20:12:51 +0530

    Updated amsd.sh

--- amsd.sh
+++ amsd.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
 sum=`expr $1 + $2`
 sub=`expr $1 - $2`
 mul=`expr $1 \* $2`
 div=`expr $1 / $2`
-echo "The sum of $no1 and $no2 is $sum"
-echo "The sub btw $no1 and $no2 is $sub"
-echo "The mul of $no1 and $no2 is $mul"
-echo "The div of $no1 and $no2 is $div"
+echo "The sum of $1 and $2 is $sum"
+echo "The sub btw $1 and $2 is $sub"
+echo "The mul of $1 and $2 is $mul"
+echo "The div of $1 and $2 is $div"
 
