eff728b85d275660034fecb09295fb0286bb4966
REUsha007 <134149384+REUsha007@users.noreply.github.com>
2023-06-08 20:24:34 +0530

    Update amsd.sh

--- amsd.sh
+++ amsd.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-sum=`expr $1 + $2`
-sub=`expr $1 - $2`
-mul=`expr $1 \* $2`
-div=`expr $1 / $2`
-echo "The sum of $1 and $2 is $sum"
-echo "The sub btw $1 and $2 is $sub"
-echo "The mul of $1 and $2 is $mul"
-echo "The div of $1 and $2 is $div"
+
+echo "The sum of $1 + $2 is"
+
 
